95b7e30560f22a6bac9b9019a9c6a8c52875fc7d
Ster-Kang <ster.ksw.kang@gmail.com>
2024-07-03 00:04:56 +0900

    AlarmRelation 컬럼 추가 / QuesionVO 수정 중

--- src/main/java/com/ksw/dto/forObject/relation/AlarmRelationDTO.java
+++ src/main/java/com/ksw/dto/forObject/relation/AlarmRelationDTO.java
@@ -5,6 +5,9 @@ public class AlarmRelationDTO {
     private Integer alarmNo;
     private Integer userNo;
     private Integer makerNo;
+    private Integer noteNo;
+    private Integer replyNo;
+    private Integer cateogryNo;
 
     // 기본 생성자
     public AlarmRelationDTO() {}
@@ -33,12 +36,42 @@ public class AlarmRelationDTO {
     public void setMakerNo(Integer makerNo) {
         this.makerNo = makerNo;
     }
+    
 
-    // 빌더 패턴 구현
+    public Integer getNoteNo() {
+		return noteNo;
+	}
+
+	public Integer getReplyNo() {
+		return replyNo;
+	}
+
+	public Integer getCateogryNo() {
+		return cateogryNo;
+	}
+
+	public void setNoteNo(Integer noteNo) {
+		this.noteNo = noteNo;
+	}
+
+	public void setReplyNo(Integer replyNo) {
+		this.replyNo = replyNo;
+	}
+
+	public void setCateogryNo(Integer cateogryNo) {
+		this.cateogryNo = cateogryNo;
+	}
+
+
+
+	// 빌더 패턴 구현
     public static class Builder {
         private Integer alarmNo;
         private Integer userNo;
         private Integer makerNo;
+        private Integer noteNo;
+        private Integer replyNo;
+        private Integer cateogryNo;
 
         public Builder alarmNo(Integer alarmNo) {
             this.alarmNo = alarmNo;
@@ -54,12 +87,29 @@ public class AlarmRelationDTO {
             this.makerNo = makerNo;
             return this;
         }
+        
+        public Builder noteNo(Integer noteNo) {
+            this.noteNo = noteNo;
+            return this;
+        }
+        
+        public Builder replyNo(Integer replyNo) {
+            this.replyNo = replyNo;
+            return this;
+        }
+        public Builder cateogryNo(Integer cateogryNo) {
+            this.cateogryNo = cateogryNo;
+            return this;
+        }
 
         public AlarmRelationDTO build() {
             AlarmRelationDTO alarmRelationDTO = new AlarmRelationDTO();
             alarmRelationDTO.alarmNo = this.alarmNo;
             alarmRelationDTO.userNo = this.userNo;
             alarmRelationDTO.makerNo = this.makerNo;
+            alarmRelationDTO.noteNo = this.noteNo;
+            alarmRelationDTO.replyNo = this.replyNo;
+            alarmRelationDTO.cateogryNo = this.cateogryNo;
             return alarmRelationDTO;
         }
     }
