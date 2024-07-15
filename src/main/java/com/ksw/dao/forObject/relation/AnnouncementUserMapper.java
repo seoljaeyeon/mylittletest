bcdfccf10ea6969af3ff82350b8c094787afc9cc
seol <wodus8845@gmail.com>
2024-07-15 12:15:34 +0900

    필요없는페이지 삭제했음

--- src/main/java/com/ksw/dao/forObject/relation/AnnouncementUserMapper.java
+++ src/main/java/com/ksw/dao/forObject/relation/AnnouncementUserMapper.java
@@ -1,5 +1,12 @@
 package com.ksw.dao.forObject.relation;
 
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
 public interface AnnouncementUserMapper {
 
+	
+	/*
+	 * @Insert("" + "Insert INTO Ann~") Announcement insert()
+	 */
 }
--- src/main/java/com/ksw/service/forObject/entity/AnnouncementService.java
+++ src/main/java/com/ksw/service/forObject/entity/AnnouncementService.java
@@ -1,47 +1,55 @@
 package com.ksw.service.forObject.entity;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.ksw.dao.forObject.relation.AnnouncementUserMapper;
 import com.ksw.dto.forObject.entity.AnnouncementDTO;
 import com.ksw.object.entity.Announcement;
+import com.ksw.object.relation.AnnouncementUser;
 import com.ksw.vo.forObject.entity.AnnouncementVO;
 
 @Service
 public class AnnouncementService {
 
-    // Entity -> DTO 변환 메소드
-    public AnnouncementDTO convertToDTO(Announcement announcement) {
-    	AnnouncementDTO dto = new AnnouncementDTO();
-    	if (announcement == null) {
-    		System.out.println("Announcement to AnnouncementDTO failed. Empty AnnouncementDTO created. Announcement is null");
-            return dto;
-    	}
-    	dto.setAnnouncementContent(announcement.getAnnouncementContent());
-    	dto.setAnnouncementNo(announcement.getAnnouncementNo());
-    	dto.setAnnouncementTitle(announcement.getAnnouncementTitle());
-    	dto.setCreatedAt(announcement.getCreatedAt());
-    	dto.setIsActive(announcement.getIsActive());
-    	dto.setSchedule(announcement.getSchedule());
-    	dto.setUpdatedAt(announcement.getUpdatedAt());
-        return dto;
-    }
+	@Autowired
+	private AnnouncementUserMapper announcementUserMapper;
 
-    // DTO -> VO 변환 메소드
-    public AnnouncementVO convertToVO(AnnouncementDTO announcementDTO) {
-    	if (announcementDTO == null) {
-    		System.out.println("AnnouncementDTO to AnnouncementVO failed. Empty AnnouncementVO created. AnnouncementDTO is null");
-            return new AnnouncementVO.Builder().build();
-    	}
-        return new AnnouncementVO.Builder()
-                .announcementNo(announcementDTO.getAnnouncementNo())
-                .announcementTitle(announcementDTO.getAnnouncementTitle())
-                .announcementContent(announcementDTO.getAnnouncementContent())
-                .schedule(announcementDTO.getSchedule())
-                .isActive(announcementDTO.getIsActive())
-                .createdAt(announcementDTO.getCreatedAt())
-                .updatedAt(announcementDTO.getUpdatedAt())
-                .build();
-    }
-    
-    
+	/*
+	 * public Announcement insertAnnouncement(AnnouncementUser a) {
+	 * announcementUserMapper.insert(a); }
+	 */
+
+	// Entity -> DTO 변환 메소드
+	public AnnouncementDTO convertToDTO(Announcement announcement) {
+		AnnouncementDTO dto = new AnnouncementDTO();
+		if (announcement == null) {
+			System.out.println(
+					"Announcement to AnnouncementDTO failed. Empty AnnouncementDTO created. Announcement is null");
+			return dto;
+		}
+		dto.setAnnouncementContent(announcement.getAnnouncementContent());
+		dto.setAnnouncementNo(announcement.getAnnouncementNo());
+		dto.setAnnouncementTitle(announcement.getAnnouncementTitle());
+		dto.setCreatedAt(announcement.getCreatedAt());
+		dto.setIsActive(announcement.getIsActive());
+		dto.setSchedule(announcement.getSchedule());
+		dto.setUpdatedAt(announcement.getUpdatedAt());
+		return dto;
+	}
+
+	// DTO -> VO 변환 메소드
+	public AnnouncementVO convertToVO(AnnouncementDTO announcementDTO) {
+		if (announcementDTO == null) {
+			System.out.println(
+					"AnnouncementDTO to AnnouncementVO failed. Empty AnnouncementVO created. AnnouncementDTO is null");
+			return new AnnouncementVO.Builder().build();
+		}
+		return new AnnouncementVO.Builder().announcementNo(announcementDTO.getAnnouncementNo())
+				.announcementTitle(announcementDTO.getAnnouncementTitle())
+				.announcementContent(announcementDTO.getAnnouncementContent()).schedule(announcementDTO.getSchedule())
+				.isActive(announcementDTO.getIsActive()).createdAt(announcementDTO.getCreatedAt())
+				.updatedAt(announcementDTO.getUpdatedAt()).build();
+	}
+
 }
